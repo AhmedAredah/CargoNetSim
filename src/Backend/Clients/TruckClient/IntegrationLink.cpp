49afb0bf0ae02bf5803dfa2e687806a6ffe64bd0
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2025-03-22 18:49:33 -0400

    feat(truck-client): add IntegrationLink and IntegrationNode classes
    Implements IntegrationLink and IntegrationNode classes to model transportation network links and nodes.
    
    Includes JSON serialization and deserialization methods, property setters, and signal emission for changes.
    
    Enhances the truck client backend with detailed traffic and spatial properties.

---
+++ src/Backend/Clients/TruckClient/IntegrationLink.cpp
@@ -0,0 +1,385 @@
+/**
+ * @file IntegrationLink.cpp
+ * @brief Implements transportation network link
+ * @author [Your Name]
+ * @date 2025-03-22
+ */
+
+#include "IntegrationLink.h"
+
+namespace CargoNetSim {
+namespace Backend {
+namespace TruckClient {
+
+IntegrationLink::IntegrationLink(QObject *parent)
+    : QObject(parent)
+    , m_linkId(0)
+    , m_upstreamNodeId(0)
+    , m_downstreamNodeId(0)
+    , m_length(0.0f)
+    , m_freeSpeed(0.0f)
+    , m_saturationFlow(0.0f)
+    , m_lanes(0.0f)
+    , m_speedCoeffVariation(0.0f)
+    , m_speedAtCapacity(0.0f)
+    , m_jamDensity(0.0f)
+    , m_turnProhibition(0)
+    , m_prohibitionStart(0)
+    , m_prohibitionEnd(0)
+    , m_opposingLink1(0)
+    , m_opposingLink2(0)
+    , m_trafficSignal(0)
+    , m_phase1(0)
+    , m_phase2(0)
+    , m_vehicleClassProhibition(0)
+    , m_surveillanceLevel(0)
+    , m_description("")
+    , m_lengthScale(1.0f)
+    , m_speedScale(1.0f)
+    , m_saturationFlowScale(1.0f)
+    , m_speedAtCapacityScale(1.0f)
+    , m_jamDensityScale(1.0f) {}
+
+IntegrationLink::IntegrationLink(
+    int linkId, int upstreamNodeId, int downstreamNodeId,
+    float length, float freeSpeed, float saturationFlow,
+    float lanes, float speedCoeffVariation,
+    float speedAtCapacity, float jamDensity,
+    int turnProhibition, int prohibitionStart,
+    int prohibitionEnd, int opposingLink1,
+    int opposingLink2, int trafficSignal, int phase1,
+    int phase2, int vehicleClassProhibition,
+    int surveillanceLevel, const QString &description,
+    float lengthScale, float speedScale,
+    float saturationFlowScale, float speedAtCapacityScale,
+    float jamDensityScale, QObject *parent)
+    : QObject(parent)
+    , m_linkId(linkId)
+    , m_upstreamNodeId(upstreamNodeId)
+    , m_downstreamNodeId(downstreamNodeId)
+    , m_length(length)
+    , m_freeSpeed(freeSpeed)
+    , m_saturationFlow(saturationFlow)
+    , m_lanes(lanes)
+    , m_speedCoeffVariation(speedCoeffVariation)
+    , m_speedAtCapacity(speedAtCapacity)
+    , m_jamDensity(jamDensity)
+    , m_turnProhibition(turnProhibition)
+    , m_prohibitionStart(prohibitionStart)
+    , m_prohibitionEnd(prohibitionEnd)
+    , m_opposingLink1(opposingLink1)
+    , m_opposingLink2(opposingLink2)
+    , m_trafficSignal(trafficSignal)
+    , m_phase1(phase1)
+    , m_phase2(phase2)
+    , m_vehicleClassProhibition(vehicleClassProhibition)
+    , m_surveillanceLevel(surveillanceLevel)
+    , m_description(description)
+    , m_lengthScale(lengthScale)
+    , m_speedScale(speedScale)
+    , m_saturationFlowScale(saturationFlowScale)
+    , m_speedAtCapacityScale(speedAtCapacityScale)
+    , m_jamDensityScale(jamDensityScale) {}
+
+IntegrationLink::IntegrationLink(const QJsonObject &json,
+                                 QObject           *parent)
+    : QObject(parent) {
+    m_linkId           = json["link_id"].toInt();
+    m_upstreamNodeId   = json["upstream_node_id"].toInt();
+    m_downstreamNodeId = json["downstream_node_id"].toInt();
+    m_length           = json["length"].toDouble();
+    m_freeSpeed        = json["free_speed"].toDouble();
+    m_saturationFlow   = json["saturation_flow"].toDouble();
+    m_lanes            = json["lanes"].toDouble();
+    m_speedCoeffVariation =
+        json["speed_coeff_variation"].toDouble();
+    m_speedAtCapacity =
+        json["speed_at_capacity"].toDouble();
+    m_jamDensity       = json["jam_density"].toDouble();
+    m_turnProhibition  = json["turn_prohibition"].toInt();
+    m_prohibitionStart = json["prohibition_start"].toInt();
+    m_prohibitionEnd   = json["prohibition_end"].toInt();
+    m_opposingLink1    = json["opposing_link_1"].toInt();
+    m_opposingLink2    = json["opposing_link_2"].toInt();
+    m_trafficSignal    = json["traffic_signal"].toInt();
+    m_phase1           = json["phase_1"].toInt();
+    m_phase2           = json["phase_2"].toInt();
+    m_vehicleClassProhibition =
+        json["vehicle_class_prohibition"].toInt();
+    m_surveillanceLevel =
+        json["surveillance_level"].toInt();
+    m_description = json["description"].toString();
+    m_lengthScale = json["length_scale"].toDouble(1.0);
+    m_speedScale  = json["speed_scale"].toDouble(1.0);
+    m_saturationFlowScale =
+        json["saturation_flow_scale"].toDouble(1.0);
+    m_speedAtCapacityScale =
+        json["speed_at_capacity_scale"].toDouble(1.0);
+    m_jamDensityScale =
+        json["jam_density_scale"].toDouble(1.0);
+}
+
+QJsonObject IntegrationLink::toDict() const {
+    QJsonObject dict;
+    dict["link_id"]               = m_linkId;
+    dict["upstream_node_id"]      = m_upstreamNodeId;
+    dict["downstream_node_id"]    = m_downstreamNodeId;
+    dict["length"]                = m_length;
+    dict["free_speed"]            = m_freeSpeed;
+    dict["saturation_flow"]       = m_saturationFlow;
+    dict["lanes"]                 = m_lanes;
+    dict["speed_coeff_variation"] = m_speedCoeffVariation;
+    dict["speed_at_capacity"]     = m_speedAtCapacity;
+    dict["jam_density"]           = m_jamDensity;
+    dict["turn_prohibition"]      = m_turnProhibition;
+    dict["prohibition_start"]     = m_prohibitionStart;
+    dict["prohibition_end"]       = m_prohibitionEnd;
+    dict["opposing_link_1"]       = m_opposingLink1;
+    dict["opposing_link_2"]       = m_opposingLink2;
+    dict["traffic_signal"]        = m_trafficSignal;
+    dict["phase_1"]               = m_phase1;
+    dict["phase_2"]               = m_phase2;
+    dict["vehicle_class_prohibition"] =
+        m_vehicleClassProhibition;
+    dict["surveillance_level"]    = m_surveillanceLevel;
+    dict["description"]           = m_description;
+    dict["length_scale"]          = m_lengthScale;
+    dict["speed_scale"]           = m_speedScale;
+    dict["saturation_flow_scale"] = m_saturationFlowScale;
+    dict["speed_at_capacity_scale"] =
+        m_speedAtCapacityScale;
+    dict["jam_density_scale"] = m_jamDensityScale;
+    return dict;
+}
+
+IntegrationLink *
+IntegrationLink::fromDict(const QJsonObject &data,
+                          QObject           *parent) {
+    return new IntegrationLink(
+        data["link_id"].toInt(),
+        data["upstream_node_id"].toInt(),
+        data["downstream_node_id"].toInt(),
+        data["length"].toDouble(),
+        data["free_speed"].toDouble(),
+        data["saturation_flow"].toDouble(),
+        data["lanes"].toDouble(),
+        data["speed_coeff_variation"].toDouble(),
+        data["speed_at_capacity"].toDouble(),
+        data["jam_density"].toDouble(),
+        data["turn_prohibition"].toInt(),
+        data["prohibition_start"].toInt(),
+        data["prohibition_end"].toInt(),
+        data["opposing_link_1"].toInt(),
+        data["opposing_link_2"].toInt(),
+        data["traffic_signal"].toInt(),
+        data["phase_1"].toInt(), data["phase_2"].toInt(),
+        data["vehicle_class_prohibition"].toInt(),
+        data["surveillance_level"].toInt(),
+        data["description"].toString(),
+        data["length_scale"].toDouble(1.0),
+        data["speed_scale"].toDouble(1.0),
+        data["saturation_flow_scale"].toDouble(1.0),
+        data["speed_at_capacity_scale"].toDouble(1.0),
+        data["jam_density_scale"].toDouble(1.0), parent);
+}
+
+void IntegrationLink::setLinkId(int linkId) {
+    if (m_linkId != linkId) {
+        m_linkId = linkId;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setUpstreamNodeId(
+    int upstreamNodeId) {
+    if (m_upstreamNodeId != upstreamNodeId) {
+        m_upstreamNodeId = upstreamNodeId;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setDownstreamNodeId(
+    int downstreamNodeId) {
+    if (m_downstreamNodeId != downstreamNodeId) {
+        m_downstreamNodeId = downstreamNodeId;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setLength(float length) {
+    if (m_length != length) {
+        m_length = length;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setFreeSpeed(float freeSpeed) {
+    if (m_freeSpeed != freeSpeed) {
+        m_freeSpeed = freeSpeed;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setSaturationFlow(
+    float saturationFlow) {
+    if (m_saturationFlow != saturationFlow) {
+        m_saturationFlow = saturationFlow;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setLanes(float lanes) {
+    if (m_lanes != lanes) {
+        m_lanes = lanes;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setSpeedCoeffVariation(
+    float speedCoeffVariation) {
+    if (m_speedCoeffVariation != speedCoeffVariation) {
+        m_speedCoeffVariation = speedCoeffVariation;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setSpeedAtCapacity(
+    float speedAtCapacity) {
+    if (m_speedAtCapacity != speedAtCapacity) {
+        m_speedAtCapacity = speedAtCapacity;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setJamDensity(float jamDensity) {
+    if (m_jamDensity != jamDensity) {
+        m_jamDensity = jamDensity;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setTurnProhibition(
+    int turnProhibition) {
+    if (m_turnProhibition != turnProhibition) {
+        m_turnProhibition = turnProhibition;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setProhibitionStart(
+    int prohibitionStart) {
+    if (m_prohibitionStart != prohibitionStart) {
+        m_prohibitionStart = prohibitionStart;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setProhibitionEnd(
+    int prohibitionEnd) {
+    if (m_prohibitionEnd != prohibitionEnd) {
+        m_prohibitionEnd = prohibitionEnd;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setOpposingLink1(int opposingLink1) {
+    if (m_opposingLink1 != opposingLink1) {
+        m_opposingLink1 = opposingLink1;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setOpposingLink2(int opposingLink2) {
+    if (m_opposingLink2 != opposingLink2) {
+        m_opposingLink2 = opposingLink2;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setTrafficSignal(int trafficSignal) {
+    if (m_trafficSignal != trafficSignal) {
+        m_trafficSignal = trafficSignal;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setPhase1(int phase1) {
+    if (m_phase1 != phase1) {
+        m_phase1 = phase1;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setPhase2(int phase2) {
+    if (m_phase2 != phase2) {
+        m_phase2 = phase2;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setVehicleClassProhibition(
+    int vehicleClassProhibition) {
+    if (m_vehicleClassProhibition
+        != vehicleClassProhibition) {
+        m_vehicleClassProhibition = vehicleClassProhibition;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setSurveillanceLevel(
+    int surveillanceLevel) {
+    if (m_surveillanceLevel != surveillanceLevel) {
+        m_surveillanceLevel = surveillanceLevel;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setDescription(
+    const QString &description) {
+    if (m_description != description) {
+        m_description = description;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setLengthScale(float lengthScale) {
+    if (m_lengthScale != lengthScale) {
+        m_lengthScale = lengthScale;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setSpeedScale(float speedScale) {
+    if (m_speedScale != speedScale) {
+        m_speedScale = speedScale;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setSaturationFlowScale(
+    float saturationFlowScale) {
+    if (m_saturationFlowScale != saturationFlowScale) {
+        m_saturationFlowScale = saturationFlowScale;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setSpeedAtCapacityScale(
+    float speedAtCapacityScale) {
+    if (m_speedAtCapacityScale != speedAtCapacityScale) {
+        m_speedAtCapacityScale = speedAtCapacityScale;
+        emit linkChanged();
+    }
+}
+
+void IntegrationLink::setJamDensityScale(
+    float jamDensityScale) {
+    if (m_jamDensityScale != jamDensityScale) {
+        m_jamDensityScale = jamDensityScale;
+        emit linkChanged();
+    }
+}
+
+} // namespace TruckClient
+} // namespace Backend
+} // namespace CargoNetSim
---
+++ src/Backend/Clients/TruckClient/IntegrationLink.h
@@ -0,0 +1,324 @@
+/**
+ * @file IntegrationLink.h
+ * @brief Defines link in transportation network
+ * @author [Your Name]
+ * @date 2025-03-22
+ */
+
+#pragma once
+
+#include <QJsonObject>
+#include <QObject>
+#include <QString>
+
+namespace CargoNetSim {
+namespace Backend {
+namespace TruckClient {
+
+/**
+ * @class IntegrationLink
+ * @brief Represents a link in the truck network
+ *
+ * Models a connection between nodes with detailed traffic
+ * properties such as speed, capacity, and signal phasing.
+ */
+class IntegrationLink : public QObject {
+    Q_OBJECT
+
+    // Link properties as Q_PROPERTY for meta-object access
+    Q_PROPERTY(int linkId READ linkId WRITE setLinkId NOTIFY
+                   linkChanged)
+    Q_PROPERTY(int upstreamNodeId READ upstreamNodeId WRITE
+                   setUpstreamNodeId NOTIFY linkChanged)
+    Q_PROPERTY(
+        int downstreamNodeId READ downstreamNodeId WRITE
+            setDownstreamNodeId NOTIFY linkChanged)
+    Q_PROPERTY(float length READ length WRITE setLength
+                   NOTIFY linkChanged)
+    Q_PROPERTY(float freeSpeed READ freeSpeed WRITE
+                   setFreeSpeed NOTIFY linkChanged)
+    Q_PROPERTY(
+        float saturationFlow READ saturationFlow WRITE
+            setSaturationFlow NOTIFY linkChanged)
+    Q_PROPERTY(float lanes READ lanes WRITE setLanes NOTIFY
+                   linkChanged)
+    Q_PROPERTY(
+        float speedCoeffVariation READ speedCoeffVariation
+            WRITE setSpeedCoeffVariation NOTIFY linkChanged)
+    Q_PROPERTY(
+        float speedAtCapacity READ speedAtCapacity WRITE
+            setSpeedAtCapacity NOTIFY linkChanged)
+    Q_PROPERTY(float jamDensity READ jamDensity WRITE
+                   setJamDensity NOTIFY linkChanged)
+    Q_PROPERTY(
+        int turnProhibition READ turnProhibition WRITE
+            setTurnProhibition NOTIFY linkChanged)
+    Q_PROPERTY(
+        int prohibitionStart READ prohibitionStart WRITE
+            setProhibitionStart NOTIFY linkChanged)
+    Q_PROPERTY(int prohibitionEnd READ prohibitionEnd WRITE
+                   setProhibitionEnd NOTIFY linkChanged)
+    Q_PROPERTY(int opposingLink1 READ opposingLink1 WRITE
+                   setOpposingLink1 NOTIFY linkChanged)
+    Q_PROPERTY(int opposingLink2 READ opposingLink2 WRITE
+                   setOpposingLink2 NOTIFY linkChanged)
+    Q_PROPERTY(int trafficSignal READ trafficSignal WRITE
+                   setTrafficSignal NOTIFY linkChanged)
+    Q_PROPERTY(int phase1 READ phase1 WRITE setPhase1 NOTIFY
+                   linkChanged)
+    Q_PROPERTY(int phase2 READ phase2 WRITE setPhase2 NOTIFY
+                   linkChanged)
+    Q_PROPERTY(int vehicleClassProhibition READ
+                   vehicleClassProhibition WRITE
+                       setVehicleClassProhibition NOTIFY
+                           linkChanged)
+    Q_PROPERTY(
+        int surveillanceLevel READ surveillanceLevel WRITE
+            setSurveillanceLevel NOTIFY linkChanged)
+    Q_PROPERTY(QString description READ description WRITE
+                   setDescription NOTIFY linkChanged)
+    Q_PROPERTY(float lengthScale READ lengthScale WRITE
+                   setLengthScale NOTIFY linkChanged)
+    Q_PROPERTY(float speedScale READ speedScale WRITE
+                   setSpeedScale NOTIFY linkChanged)
+    Q_PROPERTY(
+        float saturationFlowScale READ saturationFlowScale
+            WRITE setSaturationFlowScale NOTIFY linkChanged)
+    Q_PROPERTY(
+        float speedAtCapacityScale READ speedAtCapacityScale
+            WRITE setSpeedAtCapacityScale NOTIFY
+                linkChanged)
+    Q_PROPERTY(
+        float jamDensityScale READ jamDensityScale WRITE
+            setJamDensityScale NOTIFY linkChanged)
+
+public:
+    /**
+     * @brief Default constructor
+     * @param parent The parent QObject
+     */
+    explicit IntegrationLink(QObject *parent = nullptr);
+
+    /**
+     * @brief Parameterized constructor
+     * @param linkId Unique identifier for the link
+     * @param upstreamNodeId Source node ID
+     * @param downstreamNodeId Destination node ID
+     * @param length Length of link in km
+     * @param freeSpeed Free flow speed in km/h
+     * @param saturationFlow Maximum flow in veh/h
+     * @param lanes Number of lanes
+     * @param speedCoeffVariation Speed variation
+     * coefficient
+     * @param speedAtCapacity Speed at capacity in km/h
+     * @param jamDensity Maximum vehicle density in veh/km
+     * @param turnProhibition Turn prohibition flag
+     * @param prohibitionStart Start time of prohibition
+     * @param prohibitionEnd End time of prohibition
+     * @param opposingLink1 ID of first opposing link
+     * @param opposingLink2 ID of second opposing link
+     * @param trafficSignal Signal control type
+     * @param phase1 First signal phase
+     * @param phase2 Second signal phase
+     * @param vehicleClassProhibition Vehicle class
+     * restrictions
+     * @param surveillanceLevel Monitoring level
+     * @param description Text description of the link
+     * @param lengthScale Scale factor for length
+     * @param speedScale Scale factor for speed
+     * @param saturationFlowScale Scale factor for flow
+     * @param speedAtCapacityScale Scale factor for capacity
+     * speed
+     * @param jamDensityScale Scale factor for jam density
+     * @param parent The parent QObject
+     */
+    IntegrationLink(
+        int linkId, int upstreamNodeId,
+        int downstreamNodeId, float length, float freeSpeed,
+        float saturationFlow, float lanes,
+        float speedCoeffVariation, float speedAtCapacity,
+        float jamDensity, int turnProhibition,
+        int prohibitionStart, int prohibitionEnd,
+        int opposingLink1, int opposingLink2,
+        int trafficSignal, int phase1, int phase2,
+        int vehicleClassProhibition, int surveillanceLevel,
+        const QString &description, float lengthScale,
+        float speedScale, float saturationFlowScale,
+        float speedAtCapacityScale, float jamDensityScale,
+        QObject *parent = nullptr);
+
+    /**
+     * @brief Constructor from JSON data
+     * @param json JSON object containing link data
+     * @param parent The parent QObject
+     */
+    IntegrationLink(const QJsonObject &json,
+                    QObject           *parent = nullptr);
+
+    /**
+     * @brief Converts the link to a JSON object
+     * @return QJsonObject representation of the link
+     */
+    QJsonObject toDict() const;
+
+    /**
+     * @brief Creates a link from JSON data
+     * @param data JSON object containing link data
+     * @param parent The parent QObject
+     * @return Pointer to the new link
+     */
+    static IntegrationLink *
+    fromDict(const QJsonObject &data,
+             QObject           *parent = nullptr);
+
+    // Getters
+    int linkId() const {
+        return m_linkId;
+    }
+    int upstreamNodeId() const {
+        return m_upstreamNodeId;
+    }
+    int downstreamNodeId() const {
+        return m_downstreamNodeId;
+    }
+    float length() const {
+        return m_length;
+    }
+    float freeSpeed() const {
+        return m_freeSpeed;
+    }
+    float saturationFlow() const {
+        return m_saturationFlow;
+    }
+    float lanes() const {
+        return m_lanes;
+    }
+    float speedCoeffVariation() const {
+        return m_speedCoeffVariation;
+    }
+    float speedAtCapacity() const {
+        return m_speedAtCapacity;
+    }
+    float jamDensity() const {
+        return m_jamDensity;
+    }
+    int turnProhibition() const {
+        return m_turnProhibition;
+    }
+    int prohibitionStart() const {
+        return m_prohibitionStart;
+    }
+    int prohibitionEnd() const {
+        return m_prohibitionEnd;
+    }
+    int opposingLink1() const {
+        return m_opposingLink1;
+    }
+    int opposingLink2() const {
+        return m_opposingLink2;
+    }
+    int trafficSignal() const {
+        return m_trafficSignal;
+    }
+    int phase1() const {
+        return m_phase1;
+    }
+    int phase2() const {
+        return m_phase2;
+    }
+    int vehicleClassProhibition() const {
+        return m_vehicleClassProhibition;
+    }
+    int surveillanceLevel() const {
+        return m_surveillanceLevel;
+    }
+    QString description() const {
+        return m_description;
+    }
+    float lengthScale() const {
+        return m_lengthScale;
+    }
+    float speedScale() const {
+        return m_speedScale;
+    }
+    float saturationFlowScale() const {
+        return m_saturationFlowScale;
+    }
+    float speedAtCapacityScale() const {
+        return m_speedAtCapacityScale;
+    }
+    float jamDensityScale() const {
+        return m_jamDensityScale;
+    }
+
+    // Setters
+    void setLinkId(int linkId);
+    void setUpstreamNodeId(int upstreamNodeId);
+    void setDownstreamNodeId(int downstreamNodeId);
+    void setLength(float length);
+    void setFreeSpeed(float freeSpeed);
+    void setSaturationFlow(float saturationFlow);
+    void setLanes(float lanes);
+    void setSpeedCoeffVariation(float speedCoeffVariation);
+    void setSpeedAtCapacity(float speedAtCapacity);
+    void setJamDensity(float jamDensity);
+    void setTurnProhibition(int turnProhibition);
+    void setProhibitionStart(int prohibitionStart);
+    void setProhibitionEnd(int prohibitionEnd);
+    void setOpposingLink1(int opposingLink1);
+    void setOpposingLink2(int opposingLink2);
+    void setTrafficSignal(int trafficSignal);
+    void setPhase1(int phase1);
+    void setPhase2(int phase2);
+    void
+    setVehicleClassProhibition(int vehicleClassProhibition);
+    void setSurveillanceLevel(int surveillanceLevel);
+    void setDescription(const QString &description);
+    void setLengthScale(float lengthScale);
+    void setSpeedScale(float speedScale);
+    void setSaturationFlowScale(float saturationFlowScale);
+    void
+    setSpeedAtCapacityScale(float speedAtCapacityScale);
+    void setJamDensityScale(float jamDensityScale);
+
+signals:
+    /**
+     * @brief Signal emitted when any link property changes
+     */
+    void linkChanged();
+
+private:
+    int m_linkId;           ///< Unique link identifier
+    int m_upstreamNodeId;   ///< Source node identifier
+    int m_downstreamNodeId; ///< Destination node identifier
+    float m_length;         ///< Link length in km
+    float m_freeSpeed;      ///< Free flow speed in km/h
+    float m_saturationFlow; ///< Maximum flow in veh/h
+    float m_lanes;          ///< Number of lanes
+    float m_speedCoeffVariation; ///< Speed variation
+                                 ///< coefficient
+    float m_speedAtCapacity;  ///< Speed at capacity in km/h
+    float m_jamDensity;       ///< Max density in veh/km
+    int   m_turnProhibition;  ///< Turn prohibition flag
+    int   m_prohibitionStart; ///< Start time of prohibition
+    int   m_prohibitionEnd;   ///< End time of prohibition
+    int   m_opposingLink1;    ///< ID of first opposing link
+    int   m_opposingLink2; ///< ID of second opposing link
+    int   m_trafficSignal; ///< Signal control type
+    int   m_phase1;        ///< First signal phase
+    int   m_phase2;        ///< Second signal phase
+    int   m_vehicleClassProhibition; ///< Vehicle class
+                                     ///< restrictions
+    int     m_surveillanceLevel;     ///< Monitoring level
+    QString m_description;           ///< Link description
+    float   m_lengthScale; ///< Scale factor for length
+    float   m_speedScale;  ///< Scale factor for speed
+    float m_saturationFlowScale;  ///< Scale factor for flow
+    float m_speedAtCapacityScale; ///< Factor for capacity
+                                  ///< speed
+    float m_jamDensityScale; ///< Factor for jam density
+};
+
+} // namespace TruckClient
+} // namespace Backend
+} // namespace CargoNetSim
