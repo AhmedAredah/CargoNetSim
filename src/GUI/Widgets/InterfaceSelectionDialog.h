4d62e63433708bcf0e71f2e88051841f0a4b4295
Ahmed Aredah <Ahmed.Aredah@gmail.com>
2025-04-27 01:44:44 -0400

    feat(InterfaceSelectionDialog): enhance dialog to support network type selection and update UI elements

--- src/GUI/Widgets/InterfaceSelectionDialog.h
+++ src/GUI/Widgets/InterfaceSelectionDialog.h
@@ -16,23 +16,38 @@ class InterfaceSelectionDialog : public QDialog
 {
     Q_OBJECT
 public:
+    // Define an enum for dialog types
+    enum DialogType
+    {
+        InterfaceSelection, // For
+                            // connectVisibleTerminalsByInterfaces
+        NetworkSelection // For
+                         // connectVisibleTerminalsByNetworks
+    };
+
     explicit InterfaceSelectionDialog(
-        const QSet<QString> &availableInterfaces,
+        const QSet<QString> &availableOptions,
         const QSet<QString> &visibleTerminalTypes,
-        QWidget             *parent = nullptr);
+        DialogType dialogType = InterfaceSelection,
+        QWidget   *parent     = nullptr);
     ~InterfaceSelectionDialog();
 
     QList<QString>      getSelectedInterfaces() const;
+    QList<QString>      getSelectedNetworkTypes() const;
     QMap<QString, bool> getIncludedTerminalTypes() const;
 
 private slots:
     void selectAllInterfaces();
     void deselectAllInterfaces();
+    void selectAllNetworkTypes();
+    void deselectAllNetworkTypes();
     void selectAllTerminalTypes();
     void deselectAllTerminalTypes();
 
 private:
-    QMap<QString, QCheckBox *> m_checkboxes;
+    DialogType                 m_dialogType;
+    QMap<QString, QCheckBox *> m_interfaceCheckboxes;
+    QMap<QString, QCheckBox *> m_networkTypeCheckboxes;
     QMap<QString, QCheckBox *> m_terminalTypeCheckboxes;
 };
 } // namespace GUI
